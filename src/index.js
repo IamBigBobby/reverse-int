b3dcea8ed3d19b446b0409be87102bc0872ed83a
Denis Svetleishii <denissvetleishii@outlook.com>
2022-08-03 14:23:37 +0400

    feat: complete js task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    const number = n.toString().split('');
+    let reversNumber = number.reverse();
+    if (reversNumber[reversNumber.length - 1] ===  '-'){
+        reversNumber.pop()
+    }
+    const joinRevers = reversNumber.join('');
+    return result = Number(joinRevers);
 }
+
+console.log(module.exports(-12345));
